7ad1440c934cb408842693715c0820a111f29f0c
simon <sic.heider@gmail.com>
2016-01-03 16:14:33 +0100

    deleted modbusmaster

--- ControllerSide/Main.cpp
+++ ControllerSide/Main.cpp
@@ -2,47 +2,50 @@
 
 ModbusMaster rotor(0, 1);
 ModbusMaster linear(0, 2);
+int goButton = 7;
+int readyLed = 12;
 
-void writeRegisterRotor(uint16_t adressUpper, uint16_t adressLower, uint16_t valueUpper, uint16_t valueLower)
+void writeRegisterRotor(ModbusMaster device, uint16_t adressUpper, uint16_t adressLower, uint16_t valueUpper, uint16_t valueLower)
 {
 	delay(50);
-	rotor.writeSingleRegister(adressUpper, valueUpper);
+	device.writeSingleRegister(adressUpper, valueUpper);
 	delay(50);
-	rotor.writeSingleRegister(adressLower, valueLower);
+	device.writeSingleRegister(adressLower, valueLower);
 	delay(50);
 }
+
+/*
+main process registers
+property		unit			initial value		upper		lower
+position		steps			0					1024		1025
+speed			Hz				1000				1152		1153
+mode			inc(0)/abs(1)	0					1280		1281
+function		0-4				0					1408		1409
+acceleration	s/kHz			1000				1536		1537
+deceleration	s/kHz			1000				1664		1665
+current			%%				200					1792		1793
+sequential		0/1				0					1920		1921
+dwelltime		unknown			0					2048		2049
+*/
 
 void setup()
 {
 	rotor.begin(9600);
 	linear.begin(9600);
+	pinMode(goButton, INPUT);
+	pinMode(readyLed, OUTPUT);
 	delay(5000);
 	//set rotor positioningsteps
-	writeRegisterRotor(1024, 1025, 0, 500);
-	/*
-	//set rotor operationspeed
-	writeRegisterRotor(1152, 1153, 0, 2);
-	//set rotor operationmode
-	writeRegisterRotor(1280, 1281, 0, 0);
-	//set rotor operationfunction
-	writeRegisterRotor(1408, 1409, 0, 0);
-	//set rotor acceleration/deceleration unit
-	writeRegisterRotor(654, 655, 0, 1);
-	//set rotor acceleration
-	writeRegisterRotor(1536, 1537, 0, 1000);
-	//set rotor deceleration
-	writeRegisterRotor(1664, 1665, 0, 1000);
-	//set rotor pushcurrent
-	writeRegisterRotor(1792, 1793, 0, 200);
-	//set rotor sequential positioning
-	writeRegisterRotor(1920, 1921, 0, 0);
-	//set rotor dwelltime
-	writeRegisterRotor(2048, 2049, 0, 0);
-	*/
+	writeRegisterRotor(rotor, 1024, 1025, 0, 1500);
 }
 
 void loop()
 {
-	rotor.writeSingleRegister(125, 8);
-	delay(5000);
+	digitalWrite(readyLed, HIGH);
+	if(digitalRead(goButton) == HIGH)
+	{
+		digitalWrite(readyLed, LOW);
+		rotor.writeSingleRegister(125, 8);
+		delay(5000);
+	}
 }
