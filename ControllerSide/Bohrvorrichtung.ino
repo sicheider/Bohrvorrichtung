1c5e89ea41269a899bf243be37d66a8e414d0241
Martin <Martin>
2016-02-24 14:58:00 +0100

    '2402'

--- ControllerSide/Bohrvorrichtung.ino
+++ ControllerSide/Bohrvorrichtung.ino
@@ -1,7 +1,7 @@
 #include <ModbusMaster.h>
 
-ModbusMaster rotor(0, 1);
-ModbusMaster linear(0, 2);
+ModbusMaster rotor(1, 1);
+ModbusMaster linear(1, 2);
 int goButton = 22;
 int readyLed = 52;
 int dataEnable = 36;
@@ -23,35 +23,28 @@ sequential      0/1             0                   1920        1921
 dwelltime       unknown         0                   2048        2049
 */
 
-void setup()
-{
+void setup(){
 	digitalWrite(readyLed, LOW);
-	rotor.begin(9600);
-	linear.begin(9600);
+	rotor.begin(19200);
+	linear.begin(19200);
+  Serial.begin(9600);
 	pinMode(goButton, INPUT);
 	pinMode(readyLed, OUTPUT);
-        pinMode(dataEnable, OUTPUT);
-        pinMode(recieveEnable, OUTPUT);
-        digitalWrite(dataEnable, HIGH);
-        digitalWrite(recieveEnable, HIGH);
+  pinMode(dataEnable, OUTPUT);
+  pinMode(recieveEnable, OUTPUT);
+  digitalWrite(dataEnable, HIGH);
+  digitalWrite(recieveEnable, HIGH);
 	delay(5000);
 	//set rotor positioningsteps
-	writeRegisterRotor(rotor, 1024, 1025, 0, 1500);
+	//writeRegister(rotor, 1024, 1025, 0, 1500);
 }
 
-void loop()
-{
-	digitalWrite(readyLed, HIGH);
-	if(digitalRead(goButton) == HIGH)
-	{
-		digitalWrite(readyLed, LOW);
-		rotor.writeSingleRegister(125, 8);
-		waitForRequest(rotor);
-	}
+void loop(){
+  digitalWrite(readyLed, LOW);
+	ping(rotor);
 }
 
-void writeRegisterRotor(ModbusMaster device, uint16_t adressUpper, uint16_t adressLower, uint16_t valueUpper, uint16_t valueLower)
-{
+void writeRegister(ModbusMaster device, uint16_t adressUpper, uint16_t adressLower, uint16_t valueUpper, uint16_t valueLower){
 	delay(50);
 	device.writeSingleRegister(adressUpper, valueUpper);
 	delay(50);
@@ -59,11 +52,9 @@ void writeRegisterRotor(ModbusMaster device, uint16_t adressUpper, uint16_t adre
 	delay(50);
 }
 
-uint16_t waitForRequest(ModbusMaster device)
-{
+uint16_t waitForRequest(ModbusMaster device){
 	uint16_t result;
-	while(device.getResponseBuffer(0) == 0)
-	{
+	while(device.getResponseBuffer(0) == 0){
 	}
 	result = device.getResponseBuffer(0);
 	device.clearResponseBuffer();
@@ -71,16 +62,27 @@ uint16_t waitForRequest(ModbusMaster device)
 	return result;
 }
 
-void switchRotationDirectionLinear()
-{
-	if(rotationDirectionLinear == 0)
-	{
-		writeRegisterRotor(linear, 900, 901, 0, 1);
+void switchRotationDirectionLinear(){
+	if(rotationDirectionLinear == 0){
+		writeRegister(linear, 900, 901, 0, 1);
 		rotationDirectionLinear = 1;
 	}
-	else
-	{
-		writeRegisterRotor(linear, 900, 901, 0, 0);
+	else{
+		writeRegister(linear, 900, 901, 0, 0);
 		rotationDirectionLinear = 0;
 	}
 }
+
+int ping(ModbusMaster device){
+  int result = 0;
+  device.clearResponseBuffer();
+  device.readHoldingRegisters(500, 64);
+  for(int i = 0; i < 64; i++){
+    Serial.println(device.getResponseBuffer(i));
+  }
+  if(device.getResponseBuffer(0) != 0){
+    result = 1;
+  }
+  return result;
+}
+
