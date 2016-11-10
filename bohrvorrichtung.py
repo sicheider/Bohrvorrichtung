b8c4901260cdccb3c073beb745b682c0a697b7c5
sic <sic.heider@gmail.com>
2016-11-10 11:22:27 +0100

    driller imports process data from json

--- bohrvorrichtung.py
+++ bohrvorrichtung.py
@@ -3,31 +3,45 @@ import communicationUtilities
 import os
 import commands
 import time
+import json
+import logging
 
 class Bohrvorrichtung(object):
     def __init__(self):
         self.rotor = stepperMotor.StepperMotor("rotor", 5, self)
         self.linear = stepperMotor.StepperMotor("linear", 6, self)
-        self.operationPositions = [15000, 16100, 15000]
-        self.operationSpeeds = [15000, 125, 2000]
-        self.operationModes = [1, 1, 1]
-        self.holeNumer = 32
-        self.rotorSteps = 1250
-        self.writeProcessParameters()
+        self.writeProcessData()
         self.isInterrupted = False
         self.mainLoopWaitTime = 0.1
         self.cr = communicationUtilities.CommandReceiver(self)
-        self.connections = []
-        self.ca.start()
         self.sayHello()
+        self.ca.start()
 
-    def writeProcessParameters(self):
-        self.rotor.writeOperationPosition(self.rotorSteps, 0)
-        self.rotor.writeOperationMode(0, 0)
-        for i in range(0, 3):
-            self.linear.writeOperationPosition(self.operationPositions[i], i)
-            self.linear.writeOperationSpeed(self.operationSpeeds[i], i)
-            self.linear.writeOperationMode(self.operationModes[i], i)
+    def loadProcessData(self):
+        data = open("processdata.json", "r")
+        self.processData = json.loads(data.read())
+        self.holeNumer = self.processData["holeNumer"]
+        logging.info("Loaded process data:")
+        logging.info(str(self.processData))
+        data.close()
+
+    def writeProcessData(self):
+        self.rotor.writeOperationPosition(self.processData["rotorSteps"], 0)
+        self.rotor.writeOperationSpeed(self.processData["rotorOperationSpeed"], 0)
+        self.rotor.writeOperationMode(self.processData["rotorOperationMode"], 0)
+        self.linear.writeOperationPosition(self.processData["x1"], 0)
+        self.linear.writeOperationPosition(self.processData["x2"], 1)
+        self.linear.writeOperationPosition(self.processData["x3"], 2)
+        self.linear.writeOperationSpeed(self.processData["v1"], 0)
+        self.linear.writeOperationSpeed(self.processData["v2"], 1)
+        self.linear.writeOperationSpeed(self.processData["v3"], 2)
+        self.linear.writeOperationMode(self.processData["mode1"], 0)
+        self.linear.writeOperationMode(self.processData["mode2"], 1)
+        self.linear.writeOperationMode(self.processData["mode3"], 2)
+
+    def processDataToDevice(self):
+        self.loadProcessData()
+        self.writeProcessData()
 
     def sayHello(self):
         self.linear.goHome()
